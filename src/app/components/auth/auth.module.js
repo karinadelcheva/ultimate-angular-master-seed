0553914a4e3fcf4f917b387eafe2ddf4ac0adc30
verylongchicken <k.t.delcheva@gmail.com>
2019-04-10 16:56:02 +0300

    Added contact editing contact tags and contact delete.

--- src/app/components/auth/auth.module.js
+++ src/app/components/auth/auth.module.js
@@ -9,7 +9,7 @@ angular
       apiKey: "AIzaSyCsNISt3dFx7dy5AImIIk62jDDd0OLvZK0",
       authDomain: "contacts-manager-e486f.firebaseapp.com",
       databaseURL: "https://contacts-manager-e486f.firebaseio.com",
-      storageBucket: "contacts-manager-e486f.appspot.com",
+      storageBucket: "contacts-manager-e486f.appspot.com"
     };
 
     $firebaseRefProvider
