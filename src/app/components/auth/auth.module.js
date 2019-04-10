2bd7618604833e285fc5c5e91e3d2926dbd4a0f0
verylongchicken <k.t.delcheva@gmail.com>
2019-04-10 10:57:44 +0300

    Auth completed.

--- src/app/components/auth/auth.module.js
+++ src/app/components/auth/auth.module.js
@@ -2,4 +2,19 @@ angular
   .module('components.auth', [
     'ui.router',
     'firebase'
-  ]);
+  ])
+  .config( function($firebaseRefProvider) {
+    var config = {
+      apiKey: "AIzaSyBvpsRP2GIM6dMGNxImoDz95z0qTPJ9cRI",
+      authDomain: "contacts-manager-fdf21.firebaseapp.com",
+      databaseURL: "https://contacts-manager-fdf21.firebaseio.com",
+      projectId: "contacts-manager-fdf21",
+      storageBucket: "contacts-manager-fdf21.appspot.com",
+      messagingSenderId: "463993708607"
+    };
+    $firebaseRefProvider.registerUrl({
+      default: config.databaseURL,
+      contacts: config.databaseURL + '/contacts'
+    })
+    firebase.initializeApp(config);
+  });
